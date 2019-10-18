dabe0bd7b68fb1b166694d72eb96b995a2c3a3f8
phwt <phwt.smwt@gmail.com>
2019-10-19 00:15:29 +0700

    Elements to objects

---
+++ pages/home/data.js
@@ -0,0 +1,91 @@
+data = [
+    {
+        name: "ดวงอาทิตย์",
+        type: "ดาวฤกษ์",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/0-sun.svg"
+    },
+    {
+        name: "ดาวพุธ",
+        type: "ดาวเคราะห์หิน (Terrestrial Planet)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/1-mercury.svg"
+    },
+    {
+        name: "ดาวศุกร์",
+        type: "ดาวเคราะห์หิน (Terrestrial Planet)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/2-venus.svg"
+    },
+    {
+        name: "โลก",
+        type: "ดาวเคราะห์หิน (Terrestrial Planet)",
+        details: "ดาวเคราะห์ลำดับที่สามจากดวงอาทิตย์ และเป็นวัตถุทางดาราศาสตร์เพียงหนึ่งเดียวที่ทราบว่ามีสิ่งมีชีวิต ร้อยละ 71 ของพื้นผิวโลกปกคลุมด้วยน้ำซึ่งส่วนใหญ่เป็นมหาสมุทร อีกร้อยละ 29 ที่เหลือเป็นแผ่นดิน",
+        orbital_period: 1,
+        au: 1,
+        moons: 1,
+        img: "svg/3-earth.svg"
+    }, {
+        name: "ดาวอังคาร",
+        type: "ดาวเคราะห์หิน (Terrestrial Planet)",
+        details: "ดาวเคราะห์ลำดับที่สี่จากดวงอาทิตย์ เป็นดาวเคราะห์เล็กที่สุดอันดับที่สองในระบบสุริยะรองจากดาวพุธ เป็นดาวเคราะห์หินที่มีบรรยากาศเบาบาง มีลักษณะพื้นผิวคล้ายคลึงกับทั้งหลุมอุกกาบาตบนดวงจันทร์ และภูเขาไฟ ทะเลทราย ตลอดจนน้ำแข็งขั้วดาวที่ปรากฏบนโลก",
+        orbital_period: 1.88,
+        au: 1.5,
+        moons: 2,
+        img: "svg/4-mars.svg"
+    },
+    {
+        name: "แถบดาวเคราะห์น้อย",
+        type: "แถบดาวเคราะห์น้อย",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: ""
+    },
+    {
+        name: "ดาวพฤหัสบดี",
+        type: "ดาวแก๊สยักษ์ (Gas Giant)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/5-jupiter.svg"
+    },
+    {
+        name: "ดาวเสาร์",
+        type: "ดาวแก๊สยักษ์ (Gas Giant)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/6-saturn.svg"
+    },
+    {
+        name: "ดาวยูเรนัส",
+        type: "ดาวแก๊สยักษ์ (Gas Giant)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/7-uranus.svg"
+    },
+    {
+        name: "ดาวเนปจูน",
+        type: "ดาวแก๊สยักษ์ (Gas Giant)",
+        details: "",
+        orbital_period: 0,
+        au: 0,
+        moons: 0,
+        img: "svg/8-neptune.svg"
+    }
+]
