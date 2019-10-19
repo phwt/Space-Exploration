76f89df875488522b7fc5fbdcc7e773db4c5b7a8
phwt <phwt.smwt@gmail.com>
2019-10-19 17:50:58 +0700

    Allow custom fields in overlay details

--- pages/home/script.js
+++ pages/home/script.js
@@ -4,57 +4,96 @@ let getSectionMid = () => Math.floor($(document).scrollTop() / window.innerHeigh
 let getMidpoint = (section) => (section * window.innerHeight) + (window.innerHeight / 2)
 let numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-function fadeChangeText(elem, content) {
+function fadeChange(elem, content) {
     $(elem).fadeOut(function () {
-        $(this).text(content).fadeIn()
+        $(this).html(content).fadeIn()
     })
 }
 
 function setOverlay(data) {
-    fadeChangeText("#overlay-name", data.name)
-    fadeChangeText("#overlay-type", data.type)
-    fadeChangeText("#overlay-details", data.details)
-    fadeChangeText("#overlay-orbital-period", numberWithCommas(data.orbital_period))
-    fadeChangeText("#overlay-dist-sun", data.au)
-    fadeChangeText("#overlay-moon", data.moons)
+
+    field = [{
+        title: "ระยะเวลาการโคจร",
+        value: numberWithCommas(data.orbital_period),
+        unit: "วัน (โลก)"
+    }, {
+        title: "ระยะห่างจากดวงอาทิตย์",
+        value: data.au,
+        unit: "AU"
+    }, {
+        title: "จำนวนดาวบริวาร",
+        value: data.moons,
+        unit: "ดวง"
+    }]
+
+    if (data.custom != undefined) {
+        field = [{
+            title: data.custom[0].title,
+            value: data.custom[0].value,
+            unit: data.custom[0].unit
+        }, {
+            title: data.custom[1].title,
+            value: data.custom[1].value,
+            unit: data.custom[1].unit
+        }, {
+            title: data.custom[2].title,
+            value: data.custom[2].value,
+            unit: data.custom[2].unit
+        }]
+    }
+
+    fadeChange("#overlay-name", data.name)
+    fadeChange("#overlay-type", data.type)
+    fadeChange("#overlay-details", data.details)
+    fadeChange("#overlay-field-1", '<span class="font-weight-bold">' + field[0].title + '</span><span class="field-value">' + field[0].value + '</span>' + field[0].unit)
+    fadeChange("#overlay-field-2", '<span class="font-weight-bold">' + field[1].title + '</span><span class="field-value">' + field[1].value + '</span>' + field[1].unit)
+    fadeChange("#overlay-field-3", '<span class="font-weight-bold">' + field[2].title + '</span><span class="field-value">' + field[2].value + '</span>' + field[2].unit)
+    
 }
 
 function updateSection() {
     section = getSection()
     $("#text-region").text((section > 4) ? "ระบบสุริยะชั้นนอก" : "ระบบสุริยะชั้นใน")
-    
-    try{    
-        scroll_percentage = calcPercent(getMidpoint(getSectionMid()), $(document).scrollTop() + window.innerHeight / 2 )
+
+    try {
+        scroll_percentage = calcPercent(getMidpoint(getSectionMid()), $(document).scrollTop() + window.innerHeight / 2)
         set_au = calcAU(getSectionMid())
         dist_au = scroll_percentage * (set_au.bottom - set_au.top) + set_au.top
         $("#text-dist > .au").text(Math.floor(dist_au * 100) / 100)
         $("#text-dist > .km").text(numberWithCommas(Math.floor(dist_au * 149597871)))
-    }catch(TypeError){
-        // do nothing
+    } catch (TypeError) {
+        $("#text-dist > .au").text(0)
+        $("#text-dist > .km").text(0)
     }
 
-    if(section == 0)
+    if (section == 0)
         $("#overlay").stop().fadeOut()
     else
         $("#overlay").stop().fadeIn()
 
     if (section != currentSection) {
         currentSection = section
-        try{
+        try {
             setOverlay(data[section - 1])
-        }catch(TypeError){
+        } catch (TypeError) {
             // do nothing
         }
     }
 }
 
-function calcAU(section){
-    if(section == 1)
-        return {top: 0, bottom: data[section].au}
-    return {top: data[section-1].au, bottom: data[section].au}
+function calcAU(section) {
+    if (section == 1)
+        return {
+            top: 0,
+            bottom: data[section].au
+        }
+    return {
+        top: data[section - 1].au,
+        bottom: data[section].au
+    }
 }
 
-function calcPercent(start, current){
+function calcPercent(start, current) {
     return ((current - start) / ((start + window.innerHeight) - start))
 }
 
@@ -72,7 +111,7 @@ $(document).ready(function () {
     })
 
     $(".overlay-box.readmore").click(() => {
-        alert("you clicked " + data[getSection()-1].name)
+        alert("you clicked " + data[getSection() - 1].name)
     })
 
     // Background parallax scrolling
@@ -91,7 +130,7 @@ $(document).ready(function () {
         $("#content-area").append('\
             <div class="h-100">\
             <div class="bodies" style="width: ' + body.size + '%">\
-                <a href="' + body.link + '"><img '+ ((body.glow) ? "class='glow'" : "") +' src="' + body.img + '"></a>\
+                <a href="' + body.link + '"><img ' + ((body.glow) ? "class='glow'" : "") + ' src="' + body.img + '"></a>\
             </div>\
         </div>\
         ')
