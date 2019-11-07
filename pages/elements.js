210d851d71ab528544c8dfc926fb9b7c26eed011
phwt <phwt.smwt@gmail.com>
2019-11-07 13:01:24 +0700

    Add option to hide readmore button

--- pages/elements.js
+++ pages/elements.js
@@ -201,7 +201,7 @@ class OverlayInfo extends OverlayPlanetInfo {
     return fieldElem;
   }
 
-  render() {
+  render(hide) {
     this.innerHTML = `
         <div class="row text-white" id="overlay-l" style="display: none">
             <div class="col-12 text-center overlay-box bottom" id="box-detail">
@@ -212,13 +212,13 @@ class OverlayInfo extends OverlayPlanetInfo {
                 <div class="spacing line"></div>
                 <div class="row row-info">` + this.getFields() + `</div>
             </div>
-            <div class="col-12 overlay-box readmore text-center"> อ่านเพิ่มเติม &gt; </div>
+            <div class="col-12 overlay-box readmore text-center ` + ((hide) ? 'd-none' : '') + `"> อ่านเพิ่มเติม &gt; </div>
         </div>
         `;
     this.setReadmoreEvent();
   }
 
-  reloadField(name, type, detail, link, field, custom) {
+  reloadField(name, type, detail, link, field, custom, hide) {
     this.timecalled = 0;
     this.name = name;
     this.type = type;
@@ -243,13 +243,13 @@ class OverlayInfo extends OverlayPlanetInfo {
     this.timecalled++;
 
     if (this.timecalled == 1) {
-      this.render();
+      this.render(hide);
       $(this).find('div').fadeIn();
       return;
     }
 
     $(this).find('div').fadeOut(() => {
-      this.render();
+      this.render(hide);
       $(this).find('div').fadeIn();
     });
   }
